ac3bbec1d2f94bf262490a919c6ff33ec24faadf
AdventureT <le@nepelius.at>
2023-08-09 11:41:25 +0200

    Moved TCStingPool to TPCString

--- Toshi/Source/Toshi/Strings/TPCString.h
+++ Toshi/Source/Toshi/Strings/TPCString.h
@@ -4,6 +4,45 @@
 
 namespace Toshi
 {
+	class TPooledCString;
+
+	class TCStringPool
+	{
+	public:
+		TCStringPool()
+		{
+			TCStringPool(1024, 0);
+		}
+
+		TCStringPool(int unk, int unk2);
+		TCStringPool(const char* a_szFileName, int unk, int unk2);
+
+		struct StringPool
+		{
+			int count;
+			TCString* m_szStrings;
+		};
+
+		struct StringPools
+		{
+			StringPool* m_pCStringPool;
+			int m_iCountOfPools;
+		};
+
+	public:
+
+		void Remove(TPooledCString& a_pcString);
+	protected:
+		TBOOL ReadFile(const char* a_szFileName);
+		void InitStringPool(int a_iStringCount);
+
+	private:
+		int m_iMaxSize;           // 0x0
+		int m_iStringCount;       // 0x4
+		int m_iCapacity;          // 0x8
+		StringPools m_oStringPool;// 0xC
+	};
+
 	class TPCString
 	{
 	public:
