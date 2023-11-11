5478d2e23d78e287e057cfff2bb0772e999a13a3
InfiniteC0re <inf.core0@gmail.com>
2023-11-12 02:26:16 +0500

    Fix TMath::SinCos

--- OpenBlob/Source/GameInterface/ATestState.h
+++ OpenBlob/Source/GameInterface/ATestState.h
@@ -6,23 +6,23 @@ class ATestState :
 	public Toshi::TGenericClassDerived<ATestState, AGameState, "ATestState", TMAKEVERSION(1, 0), TTRUE>
 {
 public:
-    ATestState() :
-        m_Elements(&Toshi::T2Allocator::s_GlobalAllocator, 1, 4)
-    {
-        
-    }
+	ATestState() :
+		m_Elements(&Toshi::T2Allocator::s_GlobalAllocator, 1, 4)
+	{
+		
+	}
 
 	virtual UpdateResult OnUpdate(float deltaTime) override;
 	virtual void OnRemoval() override;
-    virtual void OnInsertion() override;
-    virtual void OnDeactivate() override;
-    virtual void OnActivate() override;
+	virtual void OnInsertion() override;
+	virtual void OnDeactivate() override;
+	virtual void OnActivate() override;
 
 #ifdef TOSHI_DEBUG
-    virtual void DEBUG_RenderImGui() override;
+	virtual void DEBUG_RenderImGui() override;
 #endif
 
 private:
 	Toshi::T2GUIRectangle m_Menu;
-    Toshi::T2DynamicVector<Toshi::T2GUIRectangle*> m_Elements;
+	Toshi::T2DynamicVector<Toshi::T2GUIRectangle*> m_Elements;
 };
--- Toshi/Source/Toshi2/T2GUI/T2GUIElement.cpp
+++ Toshi/Source/Toshi2/T2GUI/T2GUIElement.cpp
@@ -52,7 +52,8 @@ namespace Toshi
 
 	void T2GUIElement::PreRender()
 	{
-		TVector2 vec1, vec2;	
+		TVector2 vec1 = { 0.0f, 0.0f };
+		TVector2 vec2 = { 0.0f, 0.0f };
 		
 		if (m_pParent != TNULL)
 		{
@@ -61,31 +62,31 @@ namespace Toshi
 
 			switch (m_Anchor)
 			{
-			case Anchor::TopRight:
+			case Anchor::BottomLeft:
 				vec1.x = vec1.x - fParentWidth * 0.5f;
 				vec1.y = vec1.y - fParentHeight * 0.5f;
-				break;
-			case Anchor::TopCenter:
-				vec1.y = vec1.y - fParentHeight * 0.5f;
-				break;
-			case Anchor::TopLeft:
-				vec1.y = vec1.y - fParentHeight * 0.5f;
-				vec1.x = fParentWidth * 0.5f + vec1.x;
-				break;
-			case Anchor::MiddleRight:
-				vec1.x = vec1.x - fParentWidth * 0.5f;
-				break;
-			case Anchor::MiddleLeft:
-				vec1.x = fParentWidth * 0.5f + vec1.x;
-				break;
-			case Anchor::BottomRight:
-				vec1.x = vec1.x - fParentWidth * 0.5f;
-				vec1.y = fParentHeight * 0.5f + vec1.y;
 				break;
 			case Anchor::BottomCenter:
+				vec1.y = vec1.y - fParentHeight * 0.5f;
+				break;
+			case Anchor::BottomRight:
+				vec1.y = vec1.y - fParentHeight * 0.5f;
+				vec1.x = fParentWidth * 0.5f + vec1.x;
+				break;
+			case Anchor::MiddleLeft:
+				vec1.x = vec1.x - fParentWidth * 0.5f;
+				break;
+			case Anchor::MiddleRight:
+				vec1.x = fParentWidth * 0.5f + vec1.x;
+				break;
+			case Anchor::TopLeft:
+				vec1.x = vec1.x - fParentWidth * 0.5f;
 				vec1.y = fParentHeight * 0.5f + vec1.y;
 				break;
-			case Anchor::BottomLeft:
+			case Anchor::TopCenter:
+				vec1.y = fParentHeight * 0.5f + vec1.y;
+				break;
+			case Anchor::TopRight:
 				vec1.x = fParentWidth * 0.5f + vec1.x;
 				vec1.y = fParentHeight * 0.5f + vec1.y;
 				break;
@@ -97,31 +98,31 @@ namespace Toshi
 
 		switch (m_Pivot)
 		{
-		case Pivot::TopRight:
+		case Pivot::BottomLeft:
 			vec2.x = fWidth * 0.5f + vec2.x;
 			vec2.y = fHeight * 0.5f + vec2.y;
 			break;
-		case Pivot::TopCenter:
+		case Pivot::BottomCenter:
 			vec2.y = fHeight * 0.5f + vec2.y;
 			break;
-		case Pivot::TopLeft:
+		case Pivot::BottomRight:
 			vec2.x = vec2.x - fWidth * 0.5f;
 			vec2.y = fHeight * 0.5f + vec2.y;
 			break;
 		case Pivot::MiddleRight:
-			vec2.x = fWidth * 0.5f + vec2.x;
-			break;
-		case Pivot::MiddleLeft:
 			vec2.x = vec2.x - fWidth * 0.5f;
 			break;
-		case Pivot::BottomRight:
+		case Pivot::MiddleLeft:
+			vec2.x = fWidth * 0.5f + vec2.x;
+			break;
+		case Pivot::TopLeft:
 			vec2.x = fWidth * 0.5f + vec2.x;
 			vec2.y = vec2.y - fHeight * 0.5f;
 			break;
-		case Pivot::BottomCenter:
+		case Pivot::TopCenter:
 			vec2.y = vec2.y - fHeight * 0.5f;
 			break;
-		case Pivot::BottomLeft:
+		case Pivot::TopRight:
 			vec2.x = vec2.x - fWidth * 0.5f;
 			vec2.y = vec2.y - fHeight * 0.5f;
 			break;
